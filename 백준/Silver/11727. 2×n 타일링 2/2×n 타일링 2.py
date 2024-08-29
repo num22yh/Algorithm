7a0269c6751741d747eef125772f0494d2f2b934
num22yh <113687179+num22yh@users.noreply.github.com>
2024-08-29 16:32:58 +0900

    Update 2×n 타일링 2.py

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,3 +1,7 @@
+# 먼저 가로에 2 또는 1을 배치한다.
+# 만약 2를 배치하는 경우는 2×1 타일 두 개를 배치하거나 2×2 타일 하나를 배치할 수 있으므로 경우의 수에 2를 곱해줘야 한다.
+# 1을 배치하는 경우는 단순히 2×1 타일 하나를 배치
+
 def count_tilings(n):
     # 기본 경우 설정
     if n == 1:
@@ -24,3 +28,25 @@ def count_tilings(n):
 # 입력 받기
 n = int(input())
 print(count_tilings(n)%10007)
+
+# 재귀로 풀면 시간초과
+
+# def count_tilings_recursive(n):
+#     # 기본 경우 설정
+#     if n == 1:
+#         return 1
+#     elif n == 2:
+#         return 3
+    
+#     # 가로에 1을 배치하는 경우 (2x1 타일 하나를 사용)
+#     result = count_tilings_recursive(n - 1)
+    
+#     # 가로에 2를 배치하는 경우
+#     # 2x1 타일 두 개를 배치하거나 2x2 타일 하나를 배치할 수 있으므로 경우의 수에 2를 곱해줌
+#     result += 2 * count_tilings_recursive(n - 2)
+    
+#     return result
+
+# 입력 받기
+# n = int(input())
+# print(count_tilings_recursive(n)%10007)
