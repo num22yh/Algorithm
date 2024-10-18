cb4282c137c5dd01a95aeeaa2f0e1c073cf108c3
num22yh <113687179+num22yh@users.noreply.github.com>
2024-10-18 15:23:19 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 118408 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,19 @@
 N = int(input())
 
-dp = [0] * (N + 1)
+dp=[0]*(N+1)
 
-for n in range(2, N + 1):
-    dp[n] = dp[n - 1] + 1
-    if n % 2 == 0:
-        dp[n] = min(dp[n], dp[n // 2] + 1)
-    if n % 3 == 0:
-        dp[n] = min(dp[n], dp[n // 3] + 1)
 
+for n in range(N+1):
+    if n==0 or n == 1:
+        dp[n] = 0
+    else :
+
+        dp[n] = 1+dp[n-1]
+    
+        if n%2 == 0 :
+            dp[n]= min(dp[n], 1+dp[n//2])
+        if n%3 == 0 :
+            dp[n]= min(dp[n], 1+dp[n//3])
+            
+    
 print(dp[N])
