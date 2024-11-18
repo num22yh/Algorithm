46efb21ad81c9383a159b95ae2dd13ab8f7cf333
num22yh <113687179+num22yh@users.noreply.github.com>
2024-11-18 14:15:47 +0900

    [Bronze II] Title: Strfry, Time: 268 ms, Memory: 23292 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -1,31 +1,44 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-public class Main {
-  public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        // n : 테스트케이스 수 (1000)
+        // 문자열1 문자열2 (길이 최대 1000)
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-    for (int i=0; i<n; i++){
-      String[] input = br.readLine().split(" ");
-      String first = input[0];
-      String second = input[1];
 
-      char[] firstArray = first.toCharArray();
-      char[] secondArray = second.toCharArray();
+        for (int i=0; i<n; i++){
 
-      Arrays.sort(firstArray);
-      Arrays.sort(secondArray);
+            int[] firstCount = new int[26];
+            int[] secondCount = new int[26];
+            String[] input = br.readLine().split(" ");
+            char[] first = input[0].toCharArray();
+            char[] second = input[1].toCharArray();
 
-      boolean result = Arrays.equals(firstArray, secondArray);
+            for (char c : first){
+                int x = c - 'a';
+                firstCount[x]++;
+            }
 
-      sb.append(Arrays.equals(firstArray, secondArray) ? "Possible\n" : "Impossible\n");
+            for (char c : second){
+                int x = c - 'a';
+                secondCount[x]++;
+            }
+
+
+            String result = (Arrays.equals(firstCount,secondCount))? "Possible" : "Impossible";
+            sb.append(result+"\n");
+        }
+
+        System.out.println(sb);
 
     }
-
-    System.out.println(sb);
-  }
 }
