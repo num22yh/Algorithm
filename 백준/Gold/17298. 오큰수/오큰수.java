3ef0dadefa91999994bb2f7ae510ef118a7fa614
num22yh <113687179+num22yh@users.noreply.github.com>
2024-12-03 17:13:52 +0900

    [Gold IV] Title: 오큰수, Time: 880 ms, Memory: 202988 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.ArrayDeque;
+import java.io.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
@@ -8,30 +8,28 @@ public class Main{
 
         int n = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
+        int[] okenSu = new int[n]; //[오큰수]
+        ArrayDeque<int[]> stack = new ArrayDeque<>(); // 인덱스, 수열의 수
 
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        ArrayDeque<Integer> toPrint = new ArrayDeque<>();
-
-        for(int i=n-1; i>-1; i--){
+        for (int i=0; i<n ; i++){
             int curNum = Integer.parseInt(input[i]);
 
-            while(!stack.isEmpty() && stack.peek()<= curNum){
+            while(!stack.isEmpty() && stack.peek()[1]<curNum){
+                // 날 오큰수로 갖는 수
+                okenSu[stack.peek()[0]] = curNum;
                 stack.pop();
+
             }
 
-            if(stack.isEmpty()){
-                toPrint.push(-1);
-            }else{ //stack.peek() > curNum
-                toPrint.push(stack.peek());
-            }
-
-            stack.push(curNum);
+            stack.push(new int[]{i,curNum});
         }
 
-        while(!toPrint.isEmpty()){
-            sb.append(toPrint.pop());
-            sb.append(" ");
+        for (int i : okenSu){
+            int num = (i==0)? -1 : i;
+            sb.append(num+" ");
         }
+
         System.out.println(sb);
+
     }
 }
