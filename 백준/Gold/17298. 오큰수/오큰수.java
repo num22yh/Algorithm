d96c84926352bf2cad96ce489457183b3529731a
num22yh <113687179+num22yh@users.noreply.github.com>
2024-12-03 17:16:42 +0900

    [Gold IV] Title: 오큰수, Time: 840 ms, Memory: 178644 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,35 +1,40 @@
 import java.util.ArrayDeque;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+import java.io.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
-        int[] okenSu = new int[n]; //[오큰수]
-        ArrayDeque<int[]> stack = new ArrayDeque<>(); // 인덱스, 수열의 수
+        int[] okenSu = new int[n];
+        Deque<int[]> stack = new ArrayDeque<>(); // [인덱스, 값]
 
-        for (int i=0; i<n ; i++){
-            int curNum = Integer.parseInt(input[i]);
-
-            while(!stack.isEmpty() && stack.peek()[1]<curNum){
-                // 날 오큰수로 갖는 수
-                okenSu[stack.peek()[0]] = curNum;
-                stack.pop();
-
-            }
-
-            stack.push(new int[]{i,curNum});
+        // 초기값을 -1로 설정
+        for (int i = 0; i < n; i++) {
+            okenSu[i] = -1;
         }
 
-        for (int i : okenSu){
-            int num = (i==0)? -1 : i;
-            sb.append(num+" ");
+        for (int i = 0; i < n; i++) {
+            int curNum = Integer.parseInt(input[i]);
+
+            while (!stack.isEmpty() && stack.peek()[1] < curNum) {
+                int index = stack.pop()[0];
+                okenSu[index] = curNum;
+            }
+
+            stack.push(new int[]{i, curNum});
+        }
+
+        for (int num : okenSu) {
+            sb.append(num).append(" ");
         }
 
         System.out.println(sb);
-
     }
 }
