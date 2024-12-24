3fb7e80f342c1fec51f927968ae9ee008802af3f
num22yh <113687179+num22yh@users.noreply.github.com>
2024-12-24 17:01:27 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,30 +1,29 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // input 입력 받기 : 다솜이의 방 번호 N(10^6)
-        String N = br.readLine();
-        // 다솜이의 방 번호에 들어있는 0~9 의 개수를 저장할 count 배열 생성
-        int[] count = new int[10];
-        // 다솜이의 방 번호를 순회하며 배열 업데이트
-        char[] roomNum = N.toCharArray();
-        for (char c : roomNum){
-            int x = c - '0';
-            count[x]++;
+
+        char[] roomNumber = br.readLine().toCharArray();
+        int[] numbers = new int[10];
+
+        for (char c : roomNumber){
+            int number = c-'0';
+            numbers[number]++;
         }
-        // 9와 6의 개수는 (둘 합 + 1)/2 로 수정
-        int temp = (count[9]+count[6]+1)/2;
-        count[9]=temp;
-        count[6]=temp;
-        // count 배열의 최대값 업데이트
-        int max=0;
-        for (int i: count){
-            if (i>max){
-                max = i;
-            }
+
+        int temp = ( numbers[6] + numbers[9] + 1 ) / 2;
+        numbers[6]= temp;
+        numbers[9]= temp;
+
+        int max =0;
+        for(int i : numbers){
+            max = max < i ? i : max;
         }
 
         System.out.println(max);
+
     }
 }
